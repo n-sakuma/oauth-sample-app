da2ec85598b3e9bb43dd2a9d82a90606011bfa5d
Naohiro Sakuma <n.sakuma@everyleaf.com>
2014-01-10 15:54:35 +0900

    Bootup Rails app (mock nextcore)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MockNextcore::Application.config.secret_key_base = 'd72906a0f9586bfd592b1ea490723b9cd8ac49567b6e47e9b4780a6f71e08e34928aec18a9c7c50a3897298cd9934c5f170623bf7e97107e06cad2ac6a5b68db'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MockNextcore::Application.config.session_store :cookie_store, key: '_mock-nextcore_session'
